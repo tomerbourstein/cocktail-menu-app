4f22a9bc1848c92aad675e88ad111603966742e1
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-08-29 22:32:43 +0300

    find liquers from database that is equal to chosen liquer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,13 +7,13 @@ const Main = () => {
   const preferences = useSelector((state) => state.input.preferences);
   const isGenerated = useSelector((state) => state.menu.generated);
 
-  for (const key in database) {
-    console.log(database[key].id);
-      if (String(database[key].id) === String(preferences.preferredAlcohol)) {
-        console.log("yes");
-      }
-  }
-
+  console.log(database);
+  console.log(preferences.preferredAlcohol);
+  const foundAlcohol = database.find(
+    (element) =>
+      element.main_liquer === preferences.preferredAlcohol.toLowerCase()
+  );
+  console.log(foundAlcohol);
   return (
     <section id="main" className={classes.main}>
       {isGenerated ? <Drink /> : <p>What Would You Like to Drink Tonight?</p>}
