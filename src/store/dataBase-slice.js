9fbebd25aaad3d633aa1df03cb2bb2dc5ad56fb1
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-08-29 23:30:31 +0300

    update db and filter cocktaild by alcohol type logic

--- src/store/dataBase-slice.js
+++ src/store/dataBase-slice.js
@@ -1,14 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const dataBaseSlice = createSlice({
   name: "dataBase",
-  initialState: { dataBase: [], liquers: [] },
+  initialState: {
+    dataBase: [],
+    liquers: [],
+    preferences: "",
+    filterdByLiquer: [],
+    randomCocktails: [],
+  },
   reducers: {
     fetchData(state, action) {
       state.dataBase = action.payload.dataBase;
-      state.liquers = action.payload.liquers.map(element => {
+      state.liquers = action.payload.liquers.map((element) => {
         return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
       });
+    },
+    setPreference(state, action) {
+      state.preferences = action.payload;
+    },
+    filterByLiquer(state, action) {
+      let foundAlcohol = state.dataBase.find(
+        (element) => element.main_liquer === action.payload.toLowerCase()
+      );
+      for (const key in foundAlcohol.cocktail) {
+        state.filterdByLiquer.push(foundAlcohol.cocktail[key]);
+      }
+
+      
     },
   },
 });
