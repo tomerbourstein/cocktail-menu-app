72e74214a8d04f2522a9966953d3e9c409bf0c57
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-10-09 12:30:07 +0300

    added images list at main window

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,36 @@
 import { useSelector } from "react-redux";
 
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ListSubheader from "@mui/material/ListSubheader";
 import classes from "./Main.module.css";
 import Properties from "./Properties";
 import Drink from "./Drink";
 const Main = (props) => {
   const isGenerated = useSelector((state) => state.menu.generated);
-  const dataBase = useSelector(state=> state.dataBase.dataBase);
+  const dataBase = useSelector((state) => state.dataBase.dataBase);
   const cocktailsToShow = useSelector(
     (state) => state.dataBase.cocktailsToShow
   );
 
-  let arr = []
-  for(const key in dataBase[2]) {
-    arr.push({cocktail:dataBase[2].cocktail})
-  }
-  console.log(arr);
+  let arr = [];
+  let imgesList = [];
+  dataBase.forEach((dbElement) => {
+    arr.push(dbElement.cocktail);
+    arr.forEach((cocktailElement) => {
+      console.log(cocktailElement.cocktail);
+      for (const item in cocktailElement) {
+        imgesList.push(cocktailElement[item].image);
+      }
+    });
+  });
+  // Shuffle array
+const shuffled = imgesList.sort(() => 0.5 - Math.random());
+
+// Get sub-array of first n elements after shuffled
+let selected = shuffled.slice(0, 6);
+  console.log(selected);
+
   const mapCocktails = cocktailsToShow.map((element) => (
     <Drink key={element.name} drink={element} />
   ));
@@ -24,7 +40,22 @@ const Main = (props) => {
       {isGenerated ? (
         <div>{mapCocktails}</div>
       ) : (
-        <p>What Would You Like to Drink Tonight?</p>
+        <>
+         <ListSubheader component="div">What Would You Like to Drink Tonight?</ListSubheader>
+          <ImageList sx={{  height: 150, mx:2 }} cols={6} rowHeight="150"
+          variant="standard ">
+            {selected.map((item, index) => (
+              <ImageListItem key={index}>
+                <img 
+                  src={`${item}?w=14&h=164&fit=crop&auto=format`}
+                  srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"random cocktails"}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </>
       )}
     </section>
   );
